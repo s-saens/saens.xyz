a54f5be3a3a550895c1406d1400133baf7c2b65d
saens <songsh5775@gmail.com>
2022-12-08 14:54:37 +0900

    name changes

--- src/lib/meta.js
+++ src/lib/meta.js
@@ -2,7 +2,7 @@
 // So they can be added as suffixes on different pages
 // Via <svelte:head>
 
-export const siteBaseUrl = 'https://fantinel.dev';
+export const siteBaseUrl = 'https://saens.xyz';
 
 export const keywords = [
 	'Saens',
