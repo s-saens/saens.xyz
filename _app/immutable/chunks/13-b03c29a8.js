4902cd55ca9b8bb1a9f7c890669ff357eea29183
saens <songsh5775@gmail.com>
2022-12-27 20:27:09 +0900

    Updates

---
+++ _app/immutable/chunks/13-b03c29a8.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/privacy/(page)/_lang_/_page.svelte-dd9e4c49.js";export{t as component};
--- _app/immutable/chunks/7-c169b2ee.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/(can-navigate)/projects/_title_/_page.svelte-3b905e33.js";export{t as component};
