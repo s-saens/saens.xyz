1d95ff1df261f77124e3b2e10717276b252ac07d
saens <songsh5775@gmail.com>
2023-05-15 10:25:32 +0900

    Updates

---
+++ _app/immutable/chunks/13-0ca3c51b.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/privacy/(page)/_lang_/_page.svelte-327a41cf.js";export{t as component};
---
+++ _app/immutable/chunks/7-d09ee786.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/(can-navigate)/projects/_title_/_page.svelte-5612b98c.js";export{t as component};
